4316850a4dea342fb85d03451e36681fd08649a0
jhonphc <100592906+jhonphc@users.noreply.github.com>
2025-05-08 23:35:51 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'd2d985b7df3d3be9d03874d6bb4ada88';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
